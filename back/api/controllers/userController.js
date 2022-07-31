c44ede6e748171b435e576f66f4e46a2398f2c00
curlynux <curlynux@gmail.com>
2022-07-31 23:44:09 +0200

    userController file written

---
+++ back/api/controllers/userController.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+var User = mongoose.model("User");
+
+exports.register = (req, res) =>
+{
+    var newUser = new User(req.body);
+    newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
+    newUser.save((err, user) => 
+    {
+        if(err) 
+            return res.status(400).send({ message: err });
+        else
+        {
+            user.hash_password = undefined;
+            return res.json(user);
+        }
+    });
+}
+
+exports.sign_in = (req, res) =>
+{
+    User.findOne({
+        email: req.body.email
+    }, (err, user) => 
+    {
+        if(err) throw err;
+        if(!user || !user.comparePassword(req.body.password))
+            return res.status(401).json({ message: "auth impossible ! invalid user ou mdp" });
+        return res.json({ token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, "RESTFULAPIs") });
+    });
+}
+
+exports.loginRequired = (req, res, next) =>
+{
+    if(req.user)
+        next();
+    else
+        return res.status(401).json({ message: "utilisateur non autorisé !" });
+}
+
+exports.profile = (req, res, next) =>
+{
+    if(req.user)
+    {
+        res.send(req.user);
+        next();
+    }
+    else
+        return res.status(401).json({ message: "token invalide" });
+}
--- back/api/models/userModel.js
+++ back/api/models/userModel.js
@@ -2,3 +2,27 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import Schema from "mongoose";
 
+var user = new Schema({
+    fullName: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        required: true
+    },
+    hash_password: {
+        type: String
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+user.methods.comparePassword = (password) => bcrypt.compareSync(password, this.hash_password);
+mongoose.model("User", user);
