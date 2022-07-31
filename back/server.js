113660dbafd06fd0658b61e23fbf8524a334195e
curlynux <curlynux@gmail.com>
2022-07-31 20:44:05 +0200

    add express server

---
+++ back/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.get("/", (req, res) => res.json({message: "salut curly !"}));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`server is listening at: ${PORT}`));
